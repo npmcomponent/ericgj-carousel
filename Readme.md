aa5e5bc6501a6255233e61c42d30c63ca2b40a3d
Eric Gjertsen <ericgj72@gmail.com>
2013-07-04 10:16:14 -0400

    debug autoplay, add test for start/stop
    add Readme
    
    remove blank carousel.css

--- Readme.md
+++ Readme.md
@@ -1,16 +1,64 @@
 
 # carousel
 
-  Carousel component
+  Another simple carousel
 
 ## Installation
 
     $ component install ericgj/carousel
 
+## Example
+
+```javascript
+  
+  var carousel = require('carousel')('#root > .item')
+  carousel.autoplay(6000);
+  window.onload = function(){
+    carousel.start();
+  }
+
+```
+
 ## API
 
-   
+### Carousel( els:NodeList|String, length )
+
+Construct a carousel of given elements, or using given selector query.
+Optionally specify a maximum length of elements, otherwise it will cycle
+through all given/selected elements.
+
+### Carousel#next
+
+Display next element
+
+### Carousel#prev
+
+Display previous element
+
+### Carousel#autoplay( msecs )
+
+Define the duration each element is displayed before advancing to next, in 
+milliseconds.
+
+### Carousel#start
+
+Start autoplay
+
+### Carousel#stop
+
+Stop autoplay
+
+
+## Notes
+
+- You should add `.hide { display: none; }` for hiding inactive elements.
+
+- For transitions, add `.transition { ... }`. Check [test][test\index.html]
+for an example.
 
 ## License
 
   MIT
+
+[test]: https://github.com/ericgj/carousel/blob/master/test/index.html
+
--- index.js
+++ index.js
@@ -6,26 +6,36 @@ function Carousel(els,length){
   if (!(this instanceof Carousel)) return new Carousel(els);
   this.els = (typeof els == 'string' ? document.querySelectorAll(els) : els);
   this.length = length || this.els.length;
+  this._init();
   return this;
 }
 
 Carousel.prototype.start = function(){
-  this.last = this.current = -1;
-  this._init();
+  this.stop();
   if (this.cycle) this.cycle();
   return this;
 }
 
+Carousel.prototype.stop = function(){
+  if (this.nextTimeout){
+    clearTimeout(this.nextTimeout);
+    delete this.nextTimeout;
+  }
+  return this;
+}
+
 Carousel.prototype.autoplay = function(msecs){
+  this.duration = msecs;
   if (msecs) {
     var self = this;
     this.cycle = function(){
       self.next();
-      setTimeout( self.cycle, msecs, self, msecs );
+      if (self.cycle) {
+        self.nextTimeout = setTimeout( self.cycle, self.duration, self, self.duration );
+      }
     }
-  } else {
-    delete this.cycle;
   }
+  return this;
 }
 
 Carousel.prototype.next = function(){
@@ -48,6 +58,7 @@ Carousel.prototype._refresh = function(){
 }
 
 Carousel.prototype._init = function(){
+  this.last = this.current = -1;
   for (var i=0;i<this.els.length;++i){
     hide(this.els[i]);
   }
